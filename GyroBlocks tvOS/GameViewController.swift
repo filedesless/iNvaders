7c76eae656331399347f36471807ca27e2880beb
Félix Larose-Gervais <filedesless@gmail.com>
2021-06-14 19:58:36 -0400

    Initial Commit

---
+++ GyroBlocks tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  GyroBlocks tvOS
+//
+//  Created by Félix Larose-Gervais on 2021-06-14.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
